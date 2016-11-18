b19c6c8c0481d7ce5e03c97257050efea3d31009
mbeatrizmagalhaes <mbeatrizmagalhaes@gmail.com>
2016-11-18 14:33:10 +0000

    Lesson 3

---
+++ src/Unit-1/DoThis/ValidationActor.cs
@@ -0,0 +1,45 @@
+﻿namespace WinTail
+{
+    using Akka.Actor;
+
+    public class ValidationActor : UntypedActor
+    {
+        private readonly IActorRef _consoleWriterActor;
+
+        public ValidationActor(IActorRef consoleWriterActor)
+        {
+            _consoleWriterActor = consoleWriterActor;
+        }
+
+
+        protected override void OnReceive(object message)
+        {
+            var msg = message as string;
+            if (string.IsNullOrEmpty(msg))
+            {
+                Self.Tell(new Messages.NullInputError("No input received."));
+            }
+            else
+            {
+                var valid = IsValid(msg);
+                if (valid)
+                {
+                    _consoleWriterActor.Tell(new Messages.InputSuccess("Thank you! Message was valid."));
+                }
+                else
+                {
+                    _consoleWriterActor.Tell(new Messages.ValidationError("Invalid: input had odd number of characters."));
+                }
+            }
+
+            // tell sender to continue its thing
+            Sender.Tell(new Messages.ContinueProcessing());
+        }
+
+        private bool IsValid(string message)
+        {
+            var valid = message.Length % 2 == 0;
+            return valid;
+        }
+    }
+}
